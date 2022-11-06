ecaae4b304bee5f68b02cb6f8227ac421edb8b81
Michael Fitzmayer <mail@michael-fitzmayer.de>
2022-11-06 15:01:02 +0100

    Add mymath.c to project sources

--- cmake/project_config.cmake
+++ cmake/project_config.cmake
@@ -6,4 +6,5 @@ set(project_sources
   ${CMAKE_CURRENT_SOURCE_DIR}/src/core.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/graphics.c
   ${CMAKE_CURRENT_SOURCE_DIR}/src/input.c
-  ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c)
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/main.c
+  ${CMAKE_CURRENT_SOURCE_DIR}/src/mymath.c)
